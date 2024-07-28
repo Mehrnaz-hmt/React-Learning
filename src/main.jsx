bf2030a008b3c344e4db2722afc62d3f8d93b24b
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-07-29 01:00:01 +0330

    S01-E5:React source files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Hi React Gangs</h1>
+}
+
+const root = document.getElementById("root")
+
+ReactDOM.createRoot(root).render(<App/>);
+
