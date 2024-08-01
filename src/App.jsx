4a34c07130debfd2887fb9eca77fa0854dbd82f1
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-08-02 00:47:01 +0330

    S01-E08:courseCard component markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,59 @@
-import "./App.css"
+import "./App.css";
 function App() {
-    return <h1>Hi React.js</h1>
+  return <div>
+    <h1>My courses</h1>
+    <div className="course-item">
+        <div className="course-item__img">
+            <img src="../public/images/js.png" alt="image-1" />
+        </div>
+        <div className="course-item__detail">
+            <div className="course-item__body">
+                <div>
+                    <p className="title">React.js course</p>
+                    <p className="desc">The ultimate React and Redux course</p>
+                </div>
+                <span className="rate">4</span>
+            </div>
+            <div className="course-item__detail">
+
+            </div>
+        </div>
+    </div>
+    <div className="course-item">
+        <div className="course-item__img">
+            <img src="../public/images/React.png" alt="image-1" />
+        </div>
+        <div className="course-item__detail">
+            <div className="course-item__body">
+                <div>
+                    <p className="title">React.js course</p>
+                    <p className="desc">The ultimate React and Redux course</p>
+                </div>
+                <span className="rate">4</span>
+            </div>
+            <div className="course-item__detail">
+
+            </div>
+        </div>
+    </div>
+    <div className="course-item">
+        <div className="course-item__img">
+            <img src="../public/images/Next.png" alt="image-1" />
+        </div>
+        <div className="course-item__detail">
+            <div className="course-item__body">
+                <div>
+                    <p className="title">React.js course</p>
+                    <p className="desc">The ultimate React and Redux course</p>
+                </div>
+                <span className="rate">4</span>
+            </div>
+            <div className="course-item__detail">
+
+            </div>
+        </div>
+    </div>
+  </div>;
 }
 
 export default App;
