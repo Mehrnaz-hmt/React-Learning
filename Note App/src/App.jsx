25e1df3459a43dd303a82eb6da82a66dea9345bb
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-08-15 00:27:59 +0330

    S03-E05:NoteList Markup

--- Note App/src/App.jsx
+++ Note App/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
-import NoteList from './components/NoteList';
+import NoteList from "./components/NoteList";
 
 function App() {
   //states
-  const [notes,setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
   //handlers
   const handleAddNote = (newNote) => {
-    setNotes(prevNotes => [...prevNotes,newNote]);
-  }
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
 
   return (
     <div className="container">
-      <div className="note-header"></div>
+      <div className="note-header">note haeder</div>
       <div className="note-app">
         <div className="add-new-note">
-          <AddNewNote onAddNote={handleAddNote}/>
+          <AddNewNote onAddNote={handleAddNote} />
         </div>
         <div className="note-container">
-          <NoteList />
+          hi
+        <NoteList notes={notes}/>
+        hi
         </div>
       </div>
     </div>
