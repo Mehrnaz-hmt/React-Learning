18470734704fecd78791f03a01adaf4be51124d1
Hemat.Mehrnaz <mehrnazhemmat1998@gmail.com>
2025-02-08 11:57:12 +0330

    S07-E15:useContext theme example

---
+++ Theme context/src/App.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+export const ThemeContext = createContext()
+
+export default function MyApp() {
+
+  const [theme, setTheme] = useState("light");
+
+  return (
+  <ThemeContext.Provider value={theme}>
+      <div>
+      <Form theme={theme} />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={(e) => {
+            setTheme(e.target.checked ? "dark" : "light");
+          }}
+        />
+        Use dark mode
+      </label>
+    </div>
+  </ThemeContext.Provider>
+  );
+}
