99dc45a8b0b6933043952426c0b398d0fed7baeb
Hemat.Mehrnaz <mehrnazhemmat1998@gmail.com>
2025-03-10 16:06:09 +0330

    React-Router-DOM

---
+++ React Router/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AppNav from "./components/AppNav";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Posts from "./pages/Posts"
+
+function App() {
+  return (
+    <div>
+      <AppNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+//SPA ==> single page application
+//popState,pushState ==>
---
+++ React Router/src/components/AppNav.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AppNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/posts">Posts</a>
+        </li>
+        <li>
+          <a href="/dashboard">Dashboard</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default AppNav;
